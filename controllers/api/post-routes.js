376c763dbdc4afef9cb8f9b5c5552ddd42ec505d
Joshua Carter <djangoisunchained99@gmail.com>
2023-01-04 14:18:12 -0500

    api cloudinary

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,5 +5,28 @@ router.get('/', async (req, res) => {
     const postData = await Post.findAll();
     return res.json(postData);
   });
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+  
+  router.post("/upload", (req, res) => {
+    console.log(req.body);
+
+    cloudinary.uploader.upload("", (error, result) => {
+      console.log("Result", result);
+      console.log("error", error);
+      if (error) {
+        res.status(500).json(error);
+        return;
+      }
+      console.log(result.url);
+      res.status(200).json(result);
+    });
+  });
+  
+  module.exports = router;
   
 module.exports = router;
