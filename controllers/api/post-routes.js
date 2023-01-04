e46b20c150cb9c28ca9cb77734714c186e9fad91
Tooboi <japica123@gmail.com>
2023-01-04 15:19:52 -0500

    upload widget and upload returnes url and song data

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
+const cloudinary = require('cloudinary');
 
 router.get('/', async (req, res) => {
     const postData = await Post.findAll();
@@ -8,23 +9,30 @@ router.get('/', async (req, res) => {
 
   cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
   
   router.post("/upload", (req, res) => {
     console.log(req.body);
 
-    cloudinary.uploader.upload("", (error, result) => {
-      console.log("Result", result);
-      console.log("error", error);
-      if (error) {
-        res.status(500).json(error);
-        return;
-      }
-      console.log(result.url);
-      res.status(200).json(result);
-    });
+    //post create needs user ID from req.session,user_id
+    //tag create
+    //posttag create 
+    //in try catch
+    
+    res.json(req.body)
+
+    // cloudinary.uploader.upload("", (error, result) => {
+    //   console.log("Result", result);
+    //   console.log("error", error);
+    //   if (error) {
+    //     res.status(500).json(error);
+    //     return;
+    //   }
+    //   console.log(result.url);
+    //   res.status(200).json(result);
+    // });
   });
   
   module.exports = router;
--- public/js/upload.js
+++ public/js/upload.js
@@ -1,45 +1,106 @@
-const api_key = '449562749791661';
-const cloud_name = 'dlvmcylti';
+// const api_key = '449562749791661';
+// const cloud_name = 'dlvmcylti';
 
-document.querySelector("#upload-form").addEventListener("submit", async function (e) {
-  e.preventDefault()
+var myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: 'dlvmcylti',
+    uploadPreset: 'soundByte',
+  },
+  async (error, result) => {
+    if (!error && result && result.event === 'success') {
+      // post request
+      console.log('Done! Here is the image info: ', result.info);
+      const song_name = document.querySelector('#track-name').value.trim();
+      const tag_name = document.querySelector('#tag-name').value.trim();
+      const audio_file = result.info.secure_url;
+      const payLoad = {
+        song_name, tag_name, audio_file
+      }
 
-  // get signature. In reality you could store this in localstorage or some other cache mechanism, it's good for 1 hour
-  const signatureResponse = await axios.get("/get-signature")
 
-  const data = new FormData()
-  data.append("file", document.querySelector("#file-field").files[0])
-  data.append("api_key", api_key)
-  data.append("signature", signatureResponse.data.signature)
-  data.append("timestamp", signatureResponse.data.timestamp)
+      const response = await fetch('/api/posts/upload', {
+        method: 'post',
+        body: JSON.stringify( payLoad ),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        console.log('new track added');
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
 
-  const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`, data, {
-    headers: { "Content-Type": "multipart/form-data" },
-    onUploadProgress: function (e) {
-      console.log(e.loaded / e.total)
+
     }
-  })
-  console.log(cloudinaryResponse.data)
-
-  // send the image info back to our server
-  const photoData = {
-    public_id: cloudinaryResponse.data.public_id,
-    version: cloudinaryResponse.data.version,
-    signature: cloudinaryResponse.data.signature
   }
+);
 
-  axios.post("/do-something-with-photo", photoData)
-})
+document.getElementById('cloudinaryUpload').addEventListener(
+  'click',
+  function (event) {
+    event.preventDefault();
+    myWidget.open();
+  },
+  false
+);
 
+// async function signupFormHandler(event) {
+//   event.preventDefault();
+
+//   if (username && email && password) {
+
+//   }
+// }
+
+// document.querySelector("#upload-form").addEventListener("submit", async function (e) {
+//   e.preventDefault()
+
+//   // get signature. In reality you could store this in localstorage or some other cache mechanism, it's good for 1 hour
+//   const signatureResponse = await axios.get("/get-signature")
+
+//   const data = new FormData()
+//   data.append("file", document.querySelector("#file-field").files[0])
+//   data.append("api_key", api_key)
+//   data.append("signature", signatureResponse.data.signature)
+//   data.append("timestamp", signatureResponse.data.timestamp)
+
+//   const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`, data, {
+//     headers: { "Content-Type": "multipart/form-data" },
+//     onUploadProgress: function (e) {
+//       console.log(e.loaded / e.total)
+//     }
+//   })
+//   console.log(cloudinaryResponse.data)
+
+//   // send the image info back to our server
+//   const photoData = {
+//     public_id: cloudinaryResponse.data.public_id,
+//     version: cloudinaryResponse.data.version,
+//     signature: cloudinaryResponse.data.signature
+//   }
+
+//   axios.post("/do-something-with-photo", photoData)
+// })
+
+// var myWidget = cloudinary.createUploadWidget({
+//   cloudName: 'dlvmcylti',
+//   uploadPreset: 'soundByte'}, (error, result) => {
+//     if (!error && result && result.event === "success") {
+//       console.log('Done! Here is the image info: ', result.info);
+//     }
+//   }
+// )
+
+// document.getElementById("cloudinaryUpload").addEventListener("click", function(){
+//     myWidget.open();
+//   }, false);
 // const widget = cloudinary.createUploadWidget(
 //   {
 //     cloudName: 'dlvmcylti',
 //     uploadPreset: 'soundByte'
 //   }
 //   )
-
-
-
 
 // function showUploadWidget() {
 //   cloudinary.openUploadWidget(
@@ -86,14 +147,11 @@ document.querySelector("#upload-form").addEventListener("submit", async function
 // }
 // document.getElementById('cloudinaryUpload').addEventListener('click', showUploadWidget() )
 
-
-
-
 // var myWidget = cloudinary.createUploadWidget({
-//   cloudName: 'dlvmcylti', 
-//   uploadPreset: 'soundByte'}, (error, result) => { 
-//     if (!error && result && result.event === "success") { 
-//       console.log('Done! Here is the image info: ', result.info); 
+//   cloudName: 'dlvmcylti',
+//   uploadPreset: 'soundByte'}, (error, result) => {
+//     if (!error && result && result.event === "success") {
+//       console.log('Done! Here is the image info: ', result.info);
 //     }
 //   }
 // )
