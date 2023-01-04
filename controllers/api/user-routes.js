1ffe0ad5d1eb6450d72717e3a31ac3e46bb1010a
Tooboi <japica123@gmail.com>
2023-01-04 13:23:57 -0500

    navbar and styles

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,6 +55,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.username = userData.username;
 
       res.json({ user: userData, message: ':) :) :)' });
     });
